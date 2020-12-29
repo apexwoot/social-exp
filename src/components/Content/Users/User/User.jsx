1b2d7f57619b7430b96c7367883d093ece317720
Vitalii Kohut <kohut9ra@gmail.com>
2020-12-29 12:02:43 +0100

    Added default profile picture with link to each profile

--- src/components/Content/Users/User/User.jsx
+++ src/components/Content/Users/User/User.jsx
@@ -1,9 +1,20 @@
 import s from './User.module.css';
+import defaultPhoto from '../../../../assets/images/avatarDefault.png';
+import { NavLink } from 'react-router-dom';
 
 const User = (props) => {
   return (
     <div className={s.userWrapper}>
       <div className={s.leftSide}>
+        {props.profilePhoto ? (
+          props.profilePhoto
+        ) : (
+          <div>
+            <NavLink to={`/profile/${props.id}`}>
+              <img alt="defaultAvatar" src={defaultPhoto} />
+            </NavLink>
+          </div>
+        )}
         <div className={s.followed}>
           {props.followed ? (
             <button onClick={() => props.unfollow(props.id)}>Unfollow</button>
